cbfe40c134c57656fb8a9da34ea0b0c7b32485ab
John Watson <johnwatson484@hotmail.com>
2021-07-21 15:18:40 +0100

    Test

--- app/agreements/get-agreements.js
+++ app/agreements/get-agreements.js
@@ -1,7 +1,7 @@
 const db = require('../data')
 
 async function getAgreements () {
-  return db.agreement.findAll({
+  return await db.agreement.findAll({
     include: [{ model: db.task, as: 'tasks', include: { model: db.status, as: 'status' } }, { model: db.paymentRequest, as: 'paymentRequests' }],
     order: [['createdAt', 'DESC']]
   })
