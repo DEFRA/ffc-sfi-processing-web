d0a11120aca8f72351012806027235d7f2a91783
Simon Dunn <simondunn90@gmail.com>
2023-09-21 15:59:54 +0100

    wip - add properties to paymentRequest object

--- app/agreements/pay-agreement.js
+++ app/agreements/pay-agreement.js
@@ -1,5 +1,6 @@
-const db = require('../data')
 const moment = require('moment')
+const { v4: uuidv4 } = require('uuid')
+const db = require('../data')
 
 async function payAgreement (agreementId) {
   const agreement = await db.agreement.findOne({
@@ -14,15 +15,22 @@ async function payAgreement (agreementId) {
     agreementId: agreement.agreementId,
     calculationData: {
       sourceSystem: 'SFIP',
+      schemeId: 1,
+      batch: undefined,
+      deliveryBody: 'RP00',
+      invoiceNumber: '',
+      frn: '',
       sbi: agreement.sbi,
       marketingYear: 2022,
       paymentRequestNumber: agreement.paymentRequests.length + 1,
       agreementNumber: agreement.agreementNumber,
       contractNumber: agreement.agreementData.contractNumber,
+      paymentType: undefined,
       currency: 'GBP',
       schedule: 'Q4',
       dueDate: moment().format('DD[/]MM[/]YYYY'),
       value: agreement.agreementData.action.paymentAmount,
+      correlationId: uuidv4(),
       invoiceLines: createInvoiceLines(agreement.agreementData.action)
     }
   }
@@ -34,9 +42,15 @@ const createInvoiceLines = (action) => {
   const invoiceLines = []
   for (const funding in action) {
     invoiceLines.push({
-      standardCode: action[funding].code,
+      schemeCode: action[funding].code,
+      accountCode: '',
+      fundCode: '',
+      agreementNumber: '',
       description: 'G00 - Gross value of claim',
-      value: action[funding].paymentAmount
+      value: action[funding].paymentAmount,
+      convergence: undefined,
+      deliveryBody: '',
+      marketingYear: ''
     })
   }
   return invoiceLines
