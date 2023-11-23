ca63f5de51f26c9ba5546ead3c25e720309c48c5
John Watson <42574977+johnwatson484@users.noreply.github.com>
2023-11-23 14:15:57 +0000

    Rebirth (#30)

--- app/agreements/pay-agreement.js
+++ app/agreements/pay-agreement.js
@@ -1,5 +1,6 @@
-const db = require('../data')
 const moment = require('moment')
+const { v4: uuidv4 } = require('uuid')
+const db = require('../data')
 
 async function payAgreement (agreementId) {
   const agreement = await db.agreement.findOne({
@@ -14,29 +15,44 @@ async function payAgreement (agreementId) {
     agreementId: agreement.agreementId,
     calculationData: {
       sourceSystem: 'SFIP',
+      schemeId: 1,
+      batch: undefined,
+      deliveryBody: 'RP00',
+      invoiceNumber: 'SFI0000001',
+      frn: '1000000001',
       sbi: agreement.sbi,
       marketingYear: 2022,
       paymentRequestNumber: agreement.paymentRequests.length + 1,
       agreementNumber: agreement.agreementNumber,
       contractNumber: agreement.agreementData.contractNumber,
+      paymentType: undefined,
       currency: 'GBP',
       schedule: 'Q4',
       dueDate: moment().format('DD[/]MM[/]YYYY'),
       value: agreement.agreementData.action.paymentAmount,
-      invoiceLines: createInvoiceLines(agreement.agreementData.action)
+      correlationId: uuidv4(),
+      invoiceLines: createInvoiceLines(agreement)
     }
   }
   await db.paymentRequest.create(paymentRequest)
   return true
 }
 
-const createInvoiceLines = (action) => {
+const createInvoiceLines = (agreement) => {
   const invoiceLines = []
-  for (const funding in action) {
+  const actions = agreement.agreementData.action
+  delete actions.paymentAmount
+  for (const funding in actions) {
     invoiceLines.push({
-      standardCode: action[funding].code,
+      schemeCode: '80101',
+      accountCode: 'SOS273',
+      fundCode: 'DRD10',
+      agreementNumber: agreement.agreementNumber,
       description: 'G00 - Gross value of claim',
-      value: action[funding].paymentAmount
+      value: actions[funding].paymentAmount,
+      convergence: undefined,
+      deliveryBody: 'RP00',
+      marketingYear: 2022
     })
   }
   return invoiceLines
